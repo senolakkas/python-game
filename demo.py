2c73fe2477619e83c0f4b75a6932f6b679d94f78
senolakkas <info@senolakkas.com>
2015-02-03 00:07:49 +0200

    ss

--- demo.py
+++ demo.py
@@ -16,6 +16,24 @@ class Game:
         self.circle = sf.CircleShape(50)
         self.circle.origin = self.circle.radius, self.circle.radius
 
+        try:
+            bg_texture = sf.Texture.from_file(
+                'assets/images/background.png')
+
+            plane_texture = sf.Texture.from_file(
+                'assets/images/planeRed1.png')
+            s_buffer = sf.SoundBuffer.from_file(
+                'assets/sounds/tone1.ogg')
+            music = sf.Music.from_file(
+                'assets/sounds/spaceTrash3.ogg')
+            font = sf.Font.from_file(
+                'assets/fonts/kenvector_future_thin.ttf')
+        except IOError:
+            print("HATA VERDİ!!")
+            sys.exit(-1)
+
+
+
     def run(self):
         while self.window.is_open:
             for event in self.window.events:
